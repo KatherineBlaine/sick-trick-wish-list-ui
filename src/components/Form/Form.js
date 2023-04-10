a349de0d3944ba581394029c681d620e968943a7
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-10 10:09:18 -0600

    feat: add options for select dropdowns in controlled form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,10 +11,17 @@ const Form = () => {
     <form>
       <select name='stance'>
         <option value=''>Choose Your Stance</option>
+        <option value="regular">Regular</option>
+        <option value="switch">Switch</option>
       </select>
       <input type='text' placeholder='Name of Trick'/>
       <select name='obstacle'>
         <option value=''>Choose Your obstacle</option>
+        <option value='flatground'>Flatground</option>
+        <option value='ledge'>Ledge</option>
+        <option value='rail'>Rail</option>
+        <option value='stairs'>Stairs</option>
+        <option value='pool'>Pool</option>
       </select>
       <input type='text' placeholder='Link to Tutorial'/>
       <button>Send it!</button>
