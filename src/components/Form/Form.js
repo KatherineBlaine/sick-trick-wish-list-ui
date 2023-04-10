ce15cc5d0c813f395d83566f2bbf7a41bf6bfcd5
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-10 10:29:31 -0600

    feat: add minor styling for form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -19,16 +19,15 @@ const Form = ( {addTrick} ) => {
     addTrick(newTrick)
   }
 
-
   return (
     <form>
-      <select name='stance' onChange={(event) => setStance(event.target.value)}>
+      <select name='stance' onChange={(event) => setStance(event.target.value)} className='input'>
         <option value=''>Choose Your Stance</option>
         <option value="regular">Regular</option>
         <option value="switch">Switch</option>
       </select>
-      <input name='name' type='text' placeholder='Name of Trick' onChange={(event) => setName(event.target.value)}/>
-      <select name='obstacle' onChange={(event) => setObstacle(event.target.value)}>
+      <input name='name' type='text' placeholder='Name of Trick' className='input' onChange={(event) => setName(event.target.value)}/>
+      <select name='obstacle' className='input' onChange={(event) => setObstacle(event.target.value)}>
         <option value=''>Choose Your obstacle</option>
         <option value='flatground'>Flatground</option>
         <option value='ledge'>Ledge</option>
@@ -36,8 +35,8 @@ const Form = ( {addTrick} ) => {
         <option value='stairs'>Stairs</option>
         <option value='pool'>Pool</option>
       </select>
-      <input name='tutorial' type='text' placeholder='Link to Tutorial' onChange={(event) => setTutorial(event.target.value)}/>
-      <button onClick={submitTrick}>Send it!</button>
+      <input name='tutorial' type='text' placeholder='Link to Tutorial' className='input' onChange={(event) => setTutorial(event.target.value)}/>
+      <button className='input' onClick={submitTrick}>Send it!</button>
     </form>
   )
 }
