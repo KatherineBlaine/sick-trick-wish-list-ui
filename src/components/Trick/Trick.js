f15809f2efecb0754fc8c68f235398438a12dde4
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-10 10:58:47 -0600

    feat: add trick card title formatting

--- src/components/Trick/Trick.js
+++ src/components/Trick/Trick.js
@@ -1,12 +1,14 @@
 import './Trick.css';
 
 const Trick = ( {name, obstacle, stance, tutorial, id} ) => {
+  const formattedName = name.charAt(0).toUpperCase() + name.slice(1)
+  const formattedStance = stance.charAt(0).toUpperCase() + stance.slice(1)
+
   return (
     <div className='card'>
-      <h3>{name}</h3>
-      <p>{obstacle}</p>
-      <p>{stance}</p>
-      <p>{tutorial}</p>
+      <p>{formattedStance} {formattedName}</p>
+      <p>Obstacle: {obstacle}</p>
+      <p>Link to Tutorial: {tutorial}</p>
     </div>
   )
 }
