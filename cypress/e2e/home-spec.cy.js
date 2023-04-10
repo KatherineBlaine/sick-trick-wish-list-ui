6f085ee12d3af0a1292c1e23f2fe30755a772420
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-10 11:03:17 -0600

    feat: complete main page DOM testing

---
+++ cypress/e2e/home-spec.cy.js
@@ -0,0 +1,38 @@
+describe('Home DOM on page load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', {
+      statusCode: 200,
+      fixture: 'trick.json'
+    })
+    .visit('http://localhost:3000/')
+  })
+
+  it('Should display the site title', () => {
+    cy.get('h1').should('be.visible').contains('Sick Trick Wish List')
+  })
+
+  it('Should display the new trick form', () => {
+    cy.get('form').should('be.visible')
+
+    cy.get('[name="stance"]').should('be.visible')
+    cy.get('[name="name"]').should('be.visible')
+    cy.get('[name="obstacle"]').should('be.visible')
+    cy.get('[name="tutorial"]').should('be.visible')
+    cy.get('button.input').should('be.visible')
+  })
+
+  it('Should display trick cards that contain the trick name, obstacle, stance, and tutorial link', () => {
+    cy.get('.card').should('be.visible')
+    
+    const trickInfo = ['flat ground', 'https://www.youtube.com/watch?v=XGw3YkQmNig', 'Obstacle:', 'Link to Tutorial:']
+
+    trickInfo.forEach(info => {
+      cy.get('.card').contains(info)
+    })
+
+    cy.get('.card > :nth-child(1)').should('be.visible')
+    cy.get('.card > :nth-child(2)').should('be.visible').contains('flat ground')
+    cy.get('.card > :nth-child(3)').should('be.visible').contains('https://www.youtube.com/watch?v=XGw3YkQmNig')
+
+  })
+})
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  // it('passes', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
 })
+
+
+
+// Write tests covering what should be displayed on the page when the user first visits.
+// - Title
+// - Form
+// - Tricks!
+
+// Write a test that checks that when data is put into the form, the value is reflected in that form input.
+// Each input field value after cy.type
+
+
+// Write a test to check the user flow of adding a new trick to the DOM.
+//
